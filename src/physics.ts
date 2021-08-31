14c7cc409df5b0992f4a6386f4a942abfca5d896
darzu <daryl.zuniga@gmail.com>
2021-08-31 22:00:00 +0000

    oct tree collision is working!

--- src/physics.ts
+++ src/physics.ts
@@ -9,6 +9,8 @@ export let _lastCollisionTestTimeMs = 0; // TODO(@darzu): hack for stat debuggin
 let _collidesWith: CollidesWith = {};
 export function checkCollisions(objs: { worldAABB: AABB, id: number }[]): CollidesWith {
     const start = performance.now()
+    _doesOverlaps = 0; // TODO(@darzu): debugging
+    _enclosedBys = 0; // TODO(@darzu): debugging
 
     // TODO(@darzu): do better than n^2. oct-tree
     // TODO(@darzu): be more precise than just AABBs. broad & narrow phases.
@@ -32,12 +34,43 @@ export function checkCollisions(objs: { worldAABB: AABB, id: number }[]): Collid
     //     }
     // }
 
-    // oct-tree
+    // naive oct-tree
     const octObjs = new Map<number, AABB>(objs.map(o => [o.id, o.worldAABB])); // TODO(@darzu): necessary?
     const maxDist = 10000;
     const octWorld: AABB = { min: [-maxDist, -maxDist, -maxDist], max: [maxDist, maxDist, maxDist] };
     const tree = octtree(octObjs, octWorld);
-    console.dir(tree)
+    function octCheckOverlap(tree: OctTree) {
+        // check ea obj
+        for (let obj of tree.objs.entries()) {
+            octObjCheckOverlap(obj, tree, true);
+        }
+        // check ea tree
+        for (let t of tree.children) {
+            if (t)
+                octCheckOverlap(t)
+        }
+    }
+    function octObjCheckOverlap(obj: [number, AABB], tree: OctTree, first = false) {
+        const [id0, box0] = obj;
+        // check this tree
+        for (let [id1, box1] of tree.objs) {
+            if ((!first || id0 < id1) && doesOverlap(box0, box1)) {
+                _collidesWith[id0].push(id1)
+                _collidesWith[id1].push(id0)
+            }
+        }
+        // check down the tree
+        for (let t of tree.children) {
+            if (t && doesOverlap(box0, t.aabb)) {
+                octObjCheckOverlap(obj, t)
+            }
+        }
+    }
+    if (tree)
+        octCheckOverlap(tree);
+
+    // TODO(@darzu): debugging
+    // console.log(debugOcttree(tree).join(','))
 
     _lastCollisionTestTimeMs = performance.now() - start;
     return _collidesWith;
@@ -49,40 +82,47 @@ interface OctTree {
 }
 const _octtreeMinLen = 1.0;
 
-function octtree(objs: Map<number, AABB>, aabb: AABB): OctTree | null {
-    const myObjs = new Map<number, AABB>();
-    for (let [id, objAABB] of objs.entries()) {
+function debugOcttree(tree: OctTree | null): number[] {
+    if (!tree)
+        return []
+    return [tree.objs.size, ...tree.children.map(debugOcttree).reduce((p, n) => [...p, ...n], [] as number[])]
+}
+
+const scratchVec3: vec3 = vec3.create();
+function octtree(parentObjs: Map<number, AABB>, aabb: AABB): OctTree | null {
+    const thisObjs = new Map<number, AABB>();
+    for (let [id, objAABB] of parentObjs.entries()) {
         if (enclosedBy(objAABB, aabb)) {
-            myObjs.set(id, objAABB)
-            objs.delete(id)
+            thisObjs.set(id, objAABB)
+            parentObjs.delete(id)
         }
     }
-    if (myObjs.size === 0)
+    if (thisObjs.size === 0)
         return null;
-    if (myObjs.size <= 2 || length <= _octtreeMinLen)
-        return { aabb, objs: myObjs, children: [null, null, null, null, null, null, null, null] }
-    const xLen = aabb.max[0] - aabb.min[0]
-    const yLen = aabb.max[1] - aabb.min[1]
-    const zLen = aabb.max[2] - aabb.min[2]
+    const nextLen = vec3.scale(scratchVec3, vec3.sub(scratchVec3, aabb.max, aabb.min), 0.5)
+    if (thisObjs.size <= 2 || nextLen[0] <= _octtreeMinLen)
+        return { aabb, objs: thisObjs, children: [null, null, null, null, null, null, null, null] }
     const childAABBs: AABB[] = [];
-    for (let xMin of [aabb.min[0], aabb.min[0] + xLen]) {
-        for (let yMin of [aabb.min[1], aabb.min[1] + yLen]) {
-            for (let zMin of [aabb.min[2], aabb.min[2] + zLen]) {
+    for (let xMin of [aabb.min[0], aabb.min[0] + nextLen[0]]) {
+        for (let yMin of [aabb.min[1], aabb.min[1] + nextLen[1]]) {
+            for (let zMin of [aabb.min[2], aabb.min[2] + nextLen[2]]) {
                 childAABBs.push({
                     min: [xMin, yMin, zMin],
-                    max: [xMin + xLen, yMin + yLen, zMin + zLen],
+                    max: [xMin + nextLen[0], yMin + nextLen[1], zMin + nextLen[2]],
                 })
             }
         }
     }
     return {
         aabb,
-        children: childAABBs.map(aabb => octtree(myObjs, aabb)),
-        objs: myObjs,
+        children: childAABBs.map(aabb => octtree(thisObjs, aabb)),
+        objs: thisObjs,
     }
 }
 // function bitree(
+export let _doesOverlaps = 0;
 export function doesOverlap(a: AABB, b: AABB) {
+    _doesOverlaps++; // TODO(@darzu): debugging
     return true
         && b.min[0] <= a.max[0]
         && b.min[1] <= a.max[1]
@@ -91,7 +131,9 @@ export function doesOverlap(a: AABB, b: AABB) {
         && a.min[1] <= b.max[1]
         && a.min[2] <= b.max[2]
 }
+export let _enclosedBys = 0;
 export function enclosedBy(inner: AABB, outer: AABB) {
+    _enclosedBys++; // TODO(@darzu): debugging
     return true
         && inner.max[0] <= outer.max[0]
         && inner.max[1] <= outer.max[1]
