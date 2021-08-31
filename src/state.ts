9da369a738fc4186ab3e718c9c4622fe17f7eae2
darzu <daryl.zuniga@gmail.com>
2021-08-31 22:00:00 +0000

    rapier is working!! (I think)

--- src/state.ts
+++ src/state.ts
@@ -229,5 +229,38 @@ function doesOverlap(a: AABB, b: AABB) {
 
 
 // import dimforgeRapier3d from 'https://cdn.skypack.dev/@dimforge/rapier3d-compat';
-import * as rapier3d from './rapier3d-dz.js';
-console.dir(rapier3d)
+import * as RAPIER from './rapier3d-dz.js';
+console.dir(RAPIER)
+
+{
+  // Use the RAPIER module here.
+  let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+  let world = new RAPIER.World(gravity);
+
+  // Create the ground
+  let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.1, 10.0);
+  world.createCollider(groundColliderDesc);
+
+  // Create a dynamic rigid-body.
+  let rigidBodyDesc = RAPIER.RigidBodyDesc.newDynamic()
+    .setTranslation(0.0, 1.0, 0.0);
+  let rigidBody = world.createRigidBody(rigidBodyDesc);
+
+  // Create a cuboid collider attached to the dynamic rigidBody.
+  let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
+  let collider = world.createCollider(colliderDesc, rigidBody.handle);
+
+  // Game loop. Replace by your own game loop system.
+  let gameLoop = () => {
+    // Ste the simulation forward.  
+    world.step();
+
+    // Get and print the rigid-body's position.
+    let position = rigidBody.translation();
+    console.log("Rigid-body position: ", position.x, position.y, position.z);
+
+    setTimeout(gameLoop, 16);
+  };
+
+  gameLoop();
+}
