b559ba34a57189bde6d2b6550f73c737a7869571
darzu <daryl.zuniga@gmail.com>
2021-08-31 22:00:00 +0000

    update max counts

--- src/main.ts
+++ src/main.ts
@@ -587,7 +587,7 @@ async function startGame(host: string | null) {
     }
   }
   if (!rendererInit) {
-    rendererInit = attachToCanvas(canvas, 1000, 100);
+    rendererInit = attachToCanvas(canvas, 20000, 6 * 2);
   }
   if (!rendererInit)
     throw 'Unable to create webgl or webgpu renderer'
