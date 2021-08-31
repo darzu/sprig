8a81eb158f7f3c8e71c552f1523d5e6dd01ad39f
Doug Woos <doug_woos@brown.edu>
2021-08-31 22:00:00 +0000

    extend note

--- src/state.ts
+++ src/state.ts
@@ -28,6 +28,14 @@ object fully once. We could instead track which nodes have seen which
 *version* of each object; we could then trigger a full sync again by 
 bumping a version number. We could use this for properties that change 
 infrequently.
+
+For objects with so much state that doing a full sync even infrequently is
+cost-prohibitive (player objects?), could also imagine a change log. Can use
+versions for this, too--a log entry is associated with a version and we sync
+nodes all log entries we think they might not have seen.
+
+For both of these, should use typescript's getters and setters to make sure
+everything gets updated in the right place.
  */
 export abstract class GameObject {
   id: number;
