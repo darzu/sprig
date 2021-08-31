fd2cdcb09c5bb01373ba7378b18dd221f80326c2
darzu <daryl.zuniga@gmail.com>
2021-08-31 22:00:00 +0000

    formatting

--- src/3d-webgpu.ts
+++ src/3d-webgpu.ts
@@ -54,25 +54,25 @@ const vertexPositionColorWGSL =
 [[stage(fragment)]]
 fn main([[location(0)]] fragUV: vec2<f32>,
         [[location(1)]] fragPosition: vec4<f32>) -> [[location(0)]] vec4<f32> {
-  return fragPosition;
+    return fragPosition;
 }
 `;
 
 const basicVertWGSL = `
 [[block]] struct Uniforms {
     modelViewProjectionMatrix : mat4x4<f32>;
-  };
-  [[binding(0), group(0)]] var<uniform> uniforms : Uniforms;
+};
+[[binding(0), group(0)]] var<uniform> uniforms : Uniforms;
 
-  struct VertexOutput {
+struct VertexOutput {
     [[builtin(position)]] Position : vec4<f32>;
     [[location(0)]] fragUV : vec2<f32>;
     [[location(1)]] fragPosition: vec4<f32>;
-  };
+};
 
-  [[stage(vertex)]]
-  fn main([[location(0)]] position : vec4<f32>,
-          [[location(1)]] uv : vec2<f32>) -> VertexOutput {
+[[stage(vertex)]]
+fn main([[location(0)]] position : vec4<f32>,
+        [[location(1)]] uv : vec2<f32>) -> VertexOutput {
     var output : VertexOutput;
     output.Position = uniforms.modelViewProjectionMatrix * position;
     output.fragUV = uv;
@@ -80,7 +80,7 @@ const basicVertWGSL = `
     // output.fragPosition = position;
 
     return output;
-  }
+}
 `;
 
 export const cubeVertexSize = 4 * 10; // Byte size of one cube vertex.
