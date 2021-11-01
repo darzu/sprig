dee5683fc1dbbbe489eafc59b84cee715bdbcabc
darzu <daryl.zuniga@gmail.com>
2021-11-01 15:25:17 -0700

    notes on colliders

---
+++ src/collider.ts
@@ -0,0 +1,4 @@
+// TODO(@darzu):
+//  we want to be able to define colliders seperate from meshes
+//  further, we want perhaps many colliders per object, and they should be parented under the objects transform
+//  perhaps most importantly, we need rotated box colliders, not just AABB
