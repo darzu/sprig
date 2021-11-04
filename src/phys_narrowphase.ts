3027e1ad9e7f3dcfcd44323766c15252a780aa4d
darzu <daryl.zuniga@gmail.com>
2021-11-04 10:45:21 -0700

    wip

---
+++ src/phys_narrowphase.ts
@@ -0,0 +1,34 @@
+// TODO(@darzu): box vs box collision testing
+// https://www.youtube.com/watch?v=ajv46BSqcK4
+
+import { quat, vec3 } from "./gl-matrix.js";
+import { AABB } from "./phys_broadphase.js";
+
+// export interface ContactData {
+//     aId: number;
+//     bId: number;
+//     bToANorm: vec3;
+//     dist: number;
+//   }
+// export interface ReboundData {
+//     aId: number;
+//     bId: number;
+//     aRebound: number;
+//     bRebound: number;
+//     aOverlap: vec3;
+//     bOverlap: vec3;
+//   }
+// function computeReboundData(
+//     a: PhysicsObject,
+//     b: PhysicsObject,
+//     itr: number
+//   ): ReboundData {
+
+interface BoxCollider {
+  motion: { location: vec3; rotation: quat };
+  worldAABB: AABB;
+}
+
+function doesOverlap(a: BoxCollider, b: BoxCollider) {
+  //
+}
