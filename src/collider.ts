eafd2624fb47d917ace8728204822db6c93b091a
darzu <daryl.zuniga@gmail.com>
2021-11-04 18:45:39 -0700

    notes updates

--- src/collider.ts
+++ src/collider.ts
@@ -1,24 +1,3 @@
-// TODO(@darzu):
-//  we want to be able to define colliders seperate from meshes
-//  further, we want perhaps many colliders per object, and they should be parented under the objects transform
-//  perhaps most importantly, we need rotated box colliders, not just AABB
-
-/*
-from Godot:
-    collision_layer
-        This describes the layers that the object appears in. By default, all bodies are on layer 1.
-    collision_mask
-        This describes what layers the body will scan for collisions. If an object isn't in one of the mask layers, the body will ignore it. By default, all bodies scan layer 1.
-
-sprig:
-    objects can have 0 or 1 collider
-    this collider can either participate in physics constraints or not
-    either way, it will generate collision events
-    if you need multiple colliders per object, either:
-    - have one or more child objects (positioned relative to u) w/ a different collider
-    - use a union composite collider type that is just one collider built out of the union of multiple other colliders (e.g. the ship)
-*/
-
 import { vec3 } from "./gl-matrix.js";
 import { AABB } from "./phys_broadphase.js";
 
--- src/phys_narrowphase.ts
+++ src/phys_narrowphase.ts
@@ -1,9 +1,11 @@
 // TODO(@darzu): box vs box collision testing
 // https://www.youtube.com/watch?v=ajv46BSqcK4
 
+import { BoxCollider } from "./collider.js";
 import { quat, vec3 } from "./gl-matrix.js";
 import { AABB } from "./phys_broadphase.js";
 
+// TODO(@darzu): interfaces worth thinking about:
 // export interface ContactData {
 //     aId: number;
 //     bId: number;
@@ -24,11 +26,22 @@ import { AABB } from "./phys_broadphase.js";
 //     itr: number
 //   ): ReboundData {
 
-interface BoxCollider {
-  motion: { location: vec3; rotation: quat };
-  world: AABB;
+function doesOverlap(a: BoxCollider, b: BoxCollider) {
+  // TODO(@darzu): implement
 }
 
-function doesOverlap(a: BoxCollider, b: BoxCollider) {
-  //
-}
+/*
+from Godot:
+    collision_layer
+        This describes the layers that the object appears in. By default, all bodies are on layer 1.
+    collision_mask
+        This describes what layers the body will scan for collisions. If an object isn't in one of the mask layers, the body will ignore it. By default, all bodies scan layer 1.
+
+sprig:
+    objects can have 0 or 1 collider
+    this collider can either participate in physics constraints or not
+    either way, it will generate collision events
+    if you need multiple colliders per object, either:
+    - have one or more child objects (positioned relative to u) w/ a different collider
+    - use a union composite collider type that is just one collider built out of the union of multiple other colliders (e.g. the ship)
+*/
