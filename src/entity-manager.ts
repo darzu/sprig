1aa22795e9537e3df7ddf765c065478f9a4fdcd3
darzu <daryl.zuniga@gmail.com>
2021-11-09 12:04:47 -0800

    starting entity manager

---
+++ src/entity-manager.ts
@@ -0,0 +1,14 @@
+/*
+entities
+components
+systems
+
+add/remove components to entities
+define a system:
+    queries over entitys w/ specifying which components
+    register with EM
+systems DAG?
+
+*/
+
+class EntityManager {}
