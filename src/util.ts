a74fde3a04dcd3ddfec816692769573d81eff367
Doug Woos <doug.woos@gmail.com>
2021-11-10 15:55:31 -0800

    Actually use hashCodes

--- src/util.ts
+++ src/util.ts
@@ -26,20 +26,15 @@ export function isString(val: any): val is string {
   return typeof val === "string";
 }
 
-(String.prototype as any).hashCode = function () {
+export function hashCode(s: string) {
   var hash = 0,
     i,
     chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr = this.charCodeAt(i);
+  if (s.length === 0) return hash;
+  for (i = 0; i < s.length; i++) {
+    chr = s.charCodeAt(i);
     hash = (hash << 5) - hash + chr;
     hash |= 0; // Convert to 32bit integer
   }
   return hash;
-};
-declare global {
-  interface String {
-    hashCode(): number;
-  }
 }
