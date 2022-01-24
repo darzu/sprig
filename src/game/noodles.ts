cd4c6ea1a9e72f104c5392fa71a0bc5d268f8070
darzu <daryl.zuniga@gmail.com>
2022-01-23 18:17:00 -0800

    noodle start

---
+++ src/game/noodles.ts
@@ -0,0 +1,21 @@
+import { Mesh } from "../mesh-pool";
+
+// function createNoodleMesh(numPoints: number): Mesh {
+//   const radius = 0.2;
+//   const segments = 6;
+// }
+
+/*
+create mesh, 
+
+#1:
+  each joint has an orientation, 
+  each vertice is assigned to a joint
+
+#2:
+  plan a whole path for the vertices
+  write the whole animation, plus a time component
+
+#3:
+  bezier curve
+*/
