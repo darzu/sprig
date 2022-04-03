1cec54a849659789616dae2e60809ae4ab8f9c27
darzu <daryl.zuniga@gmail.com>
2022-04-03 16:26:42 -0700

    remove https

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -27,7 +27,7 @@ export const DARK_BLUE = vec3.fromValues(0.03, 0.03, 0.2);
 export const LIGHT_BLUE = vec3.fromValues(0.05, 0.05, 0.2);
 
 const DEFAULT_ASSET_PATH = "/assets/";
-const BACKUP_ASSET_PATH = "https://sprig.land/assets/";
+const BACKUP_ASSET_PATH = "http://sprig.land/assets/";
 
 const RemoteMeshes = {
   ship: "barge.sprig.obj",
