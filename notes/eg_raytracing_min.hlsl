9b6a11ed9e326f8b84825ed8db1c59d72b295101
darzu <daryl.zuniga@gmail.com>
2022-04-05 10:22:55 -0700

    Daryls sprigland notes

---
+++ notes/eg_raytracing_min.hlsl
@@ -0,0 +1,153 @@
+// The MIT License
+// Copyright © 2013 Inigo Quilez
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#define AA 2 // make this 2 or 3 for antialiasing
+
+float sdBox( vec3 p, vec3 b )
+{
+    vec3 d = abs(p) - b;
+    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
+float sdPlane( vec3 p )
+{
+	return p.y;
+}
+
+float sdCone( in vec3 p, in vec2 c, float h )
+{
+    vec2 q = h*vec2(c.x,-c.y)/c.y;
+    vec2 w = vec2( length(p.xz), p.y );
+    
+	vec2 a = w - q*clamp( dot(w,q)/dot(q,q), 0.0, 1.0 );
+    vec2 b = w - q*vec2( clamp( w.x/q.x, 0.0, 1.0 ), 1.0 );
+    float k = sign( q.y );
+    float d = min(dot( a, a ),dot(b, b));
+    float s = max( k*(w.x*q.y-w.y*q.x),k*(w.y-q.y)  );
+	return sqrt(d)*sign(s);
+}
+
+vec2 opU( vec2 d1, vec2 d2 )
+{
+	return (d1.x<d2.x) ? d1 : d2;
+}
+
+#define ZERO (min(iFrame,0))
+
+vec2 map( in vec3 pos )
+{
+    vec2 res = vec2( 1e10, 0.0 );
+
+    res = opU( res, vec2( sdBox( pos-vec3(0.0,0.25, -1.0), vec3(0.6,0.5,0.2) ), 3.0 ) );
+
+    res = opU( res, vec2( sdBox( pos-vec3(0.0,-0.5, 0.0), vec3(2.0,0.01,2.0) ), 55.0 ) );
+
+    // res = opU( res, vec2( sdCone(        pos-vec3( 0.0,0.45,-1.0), vec2(0.6,0.8),0.45 ), 55.0 ) );
+
+    return res;
+}
+
+vec2 raycast( in vec3 ro, in vec3 rd )
+{
+    vec2 res = vec2(-1.0,-1.0);
+
+    float tmin = 1.0;
+    float tmax = 20.0;
+
+    float t = tmin;
+    for( int i=0; i<70 && t<tmax; i++ )
+    {
+        vec2 h = map( ro+rd*t );
+        if( abs(h.x)<(0.0001*t) )
+        { 
+            res = vec2(t,h.y); 
+            break;
+        }
+        t += h.x;
+    }
+    
+    return res;
+}
+
+vec3 calcNormal( in vec3 pos )
+{
+    vec3 n = vec3(0.0);
+    for( int i=ZERO; i<4; i++ )
+    {
+        vec3 e = 0.5773*(2.0*vec3((((i+3)>>1)&1),((i>>1)&1),(i&1))-1.0);
+        n += e*map(pos+0.0005*e).x;
+    }
+    return normalize(n);
+}
+
+vec3 render( in vec3 ro, in vec3 rd, in vec3 rdx, in vec3 rdy )
+{ 
+    // background
+    vec3 col = vec3(0.7, 0.7, 0.9) - max(rd.y,0.0)*0.3;
+    
+    // raycast scene
+    vec2 res = raycast(ro,rd);
+    float t = res.x;
+	float m = res.y;
+    if( m>-0.5 )
+    {
+        vec3 pos = ro + t*rd;
+        vec3 nor = calcNormal( pos );
+        
+        // material        
+        col = 0.2 + 0.2*sin( m*2.0 + vec3(0.0,1.0,2.0) );
+        float ks = 1.0;
+        
+        // sun
+        vec3  lig = normalize( vec3(-0.5, 0.4, -0.6) );
+        col = col*2.20*clamp( dot( nor, lig ), 0.0, 1.0 )*vec3(1.30,1.00,0.70);
+
+        col = mix( col, vec3(0.7,0.7,0.9), 1.0-exp( -0.0001*t*t*t ) );
+    }
+
+	return vec3( clamp(col,0.0,1.0) );
+}
+
+mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
+{
+	vec3 cw = normalize(ta-ro);
+	vec3 cp = vec3(sin(cr), cos(cr),0.0);
+	vec3 cu = normalize( cross(cw,cp) );
+	vec3 cv =          ( cross(cu,cw) );
+    return mat3( cu, cv, cw );
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 mo = iMouse.xy/iResolution.xy;
+	float time = 32.0 + iTime*2.5;
+
+    // camera	
+    vec3 ta = vec3( 0.5, -0.1, -0.6 );
+    vec3 ro = ta + vec3( 4.0*cos(0.1*time + 7.0*mo.x), 1.3 + 2.0*mo.y, 4.0*sin(0.1*time + 7.0*mo.x) );
+    // camera-to-world transformation
+    mat3 ca = setCamera( ro, ta, 0.0 );
+
+    vec2 p = (2.0*fragCoord-iResolution.xy)/iResolution.y;
+
+    // focal length
+    const float fl = 2.5;
+    
+    // ray direction
+    vec3 rd = ca * normalize( vec3(p,fl) );
+
+    // ray differentials
+    vec2 px = (2.0*(fragCoord+vec2(1.0,0.0))-iResolution.xy)/iResolution.y;
+    vec2 py = (2.0*(fragCoord+vec2(0.0,1.0))-iResolution.xy)/iResolution.y;
+    vec3 rdx = ca * normalize( vec3(px,fl) );
+    vec3 rdy = ca * normalize( vec3(py,fl) );
+    
+    // render	
+    vec3 col = render( ro, rd, rdx, rdy );
+    
+    // gamma
+    col = pow( col, vec3(0.4545) );
+
+    fragColor = vec4( col, 1.0 );
+}
