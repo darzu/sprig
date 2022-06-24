0b02105c4cb1af5bb106056964a0c12c71f814f3
darzu <daryl.zuniga@gmail.com>
2022-06-24 09:30:34 -0700

    remove dead code

--- src/game/xp-hyperspace.ts
+++ src/game/xp-hyperspace.ts
@@ -30,8 +30,6 @@ export function initHyperspaceGame(em: EntityManager) {
   createShip();
   // }
 
-  // create player once MeDef is present (meaning we've joined, if
-  // we're not the host)
   em.registerOneShotSystem(null, [MeDef], () => createPlayer(em));
 
   em.registerOneShotSystem(
@@ -39,109 +37,6 @@ export function initHyperspaceGame(em: EntityManager) {
     [AssetsDef, GlobalCursor3dDef, RendererDef],
     (_, res) => {
       // TODO(@darzu): call one-shot initStars
-
-      // const g = createGhost(em);
-      // vec3.copy(g.position, [0, 1, -1.2]);
-      // quat.setAxisAngle(g.rotation, [0.0, -1.0, 0.0], 1.62);
-      // g.controllable.sprintMul = 3;
-      // em.ensureComponentOn(g, ColorDef, [0.2, 0.6, 0.2]);
-      // vec3.copy(g.cameraFollow.positionOffset, [0, 0, 0]);
-      // vec3.copy(g.cameraFollow.positionOffset, [0, 2, 8]);
-
-      // TODO(@darzu): this shouldn't be necessary
-      // const m2 = cloneMesh(res.assets.cube.mesh);
-      // em.ensureComponentOn(g, RenderableConstructDef, m2, true);
-
-      {
-        // vec3.copy(g.position, [4.46, 9.61, -10.52]);
-        // quat.copy(g.rotation, [0.0, -1.0, 0.0, 0.04]);
-        // // vec3.copy(g.cameraFollow.positionOffset, [0, 0, 0]);
-        // g.cameraFollow.yawOffset = 0.0;
-        // g.cameraFollow.pitchOffset = -0.106;
-        // vec3.copy(g.position, [97.81, 0.58, -3.91]);
-        // quat.copy(g.rotation, [0.0, -0.96, 0.0, 0.29]);
-        // vec3.copy(g.cameraFollow.positionOffset, [0.0, 0.0, 0.0]);
-        // g.cameraFollow.yawOffset = 0.0;
-        // g.cameraFollow.pitchOffset = -0.522;
-        // vec3.copy(g.position, [-6.6, 0.86, 17.55]);
-        // quat.copy(g.rotation, [0.0, -0.27, 0.0, 0.96]);
-        // vec3.copy(g.cameraFollow.positionOffset, [0.0, 2.0, 8.0]);
-        // g.cameraFollow.yawOffset = 0.0;
-        // g.cameraFollow.pitchOffset = -0.536;
-        // vec3.copy(g.position, [-7.56, 0.86, 18.55]);
-        // quat.copy(g.rotation, [0.0, 0.34, 0.0, 0.93]);
-        // g.cameraFollow.yawOffset = 0.0;
-        // g.cameraFollow.pitchOffset = 0.345;
-        // vec3.copy(g.position, [-3.32, 6.69, 15.4]);
-        // quat.copy(g.rotation, [0.0, -0.27, 0.0, 0.95]);
-        // g.cameraFollow.yawOffset = 0.0;
-        // g.cameraFollow.pitchOffset = 0.142;
-      }
-
-      // const c = res.globalCursor3d.cursor()!;
-      // assert(RenderableDef.isOn(c));
-      // c.renderable.enabled = true;
-      // c.cursor3d.maxDistance = 10;
-
-      // const plane = em.newEntity();
-      // em.ensureComponentOn(
-      //   plane,
-      //   RenderableConstructDef,
-      //   res.assets.plane.proto
-      // );
-      // em.ensureComponentOn(plane, ColorDef, [0.2, 0.3, 0.2]);
-      // em.ensureComponentOn(plane, PositionDef, [0, -5, 0]);
-
-      // const fence = em.newEntity();
-      // em.ensureComponentOn(
-      //   fence,
-      //   RenderableConstructDef,
-      //   res.assets.triFence.proto
-      // );
-      // // em.ensureComponentOn(fence, ColorDef, [0.2, 0.3, 0.2]);
-      // em.ensureComponentOn(fence, ScaleDef, [2, 2, 2]);
-      // em.ensureComponentOn(fence, PositionDef, [0, 0, 10]);
-
-      // const ship = em.newEntity();
-      // em.ensureComponentOn(ship, RenderableConstructDef, res.assets.ship.proto);
-      // em.ensureComponentOn(ship, ColorDef, BOAT_COLOR);
-      // em.ensureComponentOn(ship, PositionDef, [20, -2, 0]);
-      // em.ensureComponentOn(
-      //   ship,
-      //   RotationDef,
-      //   quat.fromEuler(quat.create(), 0, Math.PI * 0.1, 0)
-      // );
-
-      // const ship2 = em.newEntity();
-      // em.ensureComponentOn(
-      //   ship2,
-      //   RenderableConstructDef,
-      //   res.assets.ship.proto
-      // );
-      // em.ensureComponentOn(ship2, ColorDef, BOAT_COLOR);
-      // em.ensureComponentOn(ship2, PositionDef, [60, -2, 0]);
-      // em.ensureComponentOn(
-      //   ship2,
-      //   RotationDef,
-      //   quat.fromEuler(quat.create(), 0, Math.PI * 0.1, 0)
-      // );
-      // em.ensureComponentOn(ship2, AngularVelocityDef, [0, 0.001, 0.001]);
-
-      // const ship3 = em.newEntity();
-      // em.ensureComponentOn(
-      //   ship3,
-      //   RenderableConstructDef,
-      //   res.assets.grappleGun.proto
-      // );
-      // em.ensureComponentOn(ship3, ColorDef, BOAT_COLOR);
-      // em.ensureComponentOn(ship3, PositionDef, [100, -2, 0]);
-      // em.ensureComponentOn(
-      //   ship3,
-      //   RotationDef,
-      //   quat.fromEuler(quat.create(), 0, Math.PI * 0.1, 0)
-      // );
-      // em.ensureComponentOn(ship3, AngularVelocityDef, [0, 0.001, 0.001]);
-
       const ocean = em.newEntity();
       em.ensureComponentOn(ocean, OceanDef);
       em.ensureComponentOn(
@@ -154,27 +49,6 @@ export function initHyperspaceGame(em: EntityManager) {
       // vec3.scale(ocean.position, ocean.position, scale);
       const scale = 100.0;
       em.ensureComponentOn(ocean, ScaleDef, [scale, scale, scale]);
-      // em.ensureComponentOn(
-      //   ocean,
-      //   RotationDef,
-      //   quat.fromEuler(quat.create(), 0, Math.PI * 0.1, 0)
-      // );
-
-      // const box = em.newEntity();
-      // const boxM = cloneMesh(res.assets.cube.mesh);
-      // const sIdMax = max(boxM.surfaceIds);
-      // // boxM.colors = boxM.surfaceIds.map((i) => uintToVec3unorm(i, sIdMax));
-      // em.ensureComponentOn(box, RenderableConstructDef, boxM);
-      // em.ensureComponentOn(box, ColorDef, [0.1, 0.1, 0.1]);
-      // em.ensureComponentOn(box, PositionDef, [0, 0, 3]);
-      // em.ensureComponentOn(box, RotationDef);
-      // em.ensureComponentOn(box, AngularVelocityDef, [0, 0.001, 0.001]);
-      // em.ensureComponentOn(box, WorldFrameDef);
-      // em.ensureComponentOn(box, ColliderDef, {
-      //   shape: "AABB",
-      //   solid: false,
-      //   aabb: res.assets.cube.aabb,
-      // });
     }
   );
 
