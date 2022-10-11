813a44572579752809ea9e41ade92d7d4f93a34d
darzu <daryl.zuniga@gmail.com>
2022-10-10 19:30:51 -0700

    fix bullet life

--- src/game/bullet.ts
+++ src/game/bullet.ts
@@ -119,7 +119,7 @@ export function createOrResetBullet(
     aabb: assets.ball.aabb,
   });
   em.ensureComponentOn(e, LifetimeDef);
-  e.lifetime.ms = 1000;
+  e.lifetime.ms = 4000;
   em.ensureComponentOn(e, SyncDef);
   e.sync.dynamicComponents = [PositionDef.id];
   e.sync.fullComponents = [BulletConstructDef.id];
