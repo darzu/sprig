a299a5cda599a1079ea38c50635dd9bdf35af1a1
darzu <daryl.zuniga@gmail.com>
2023-07-21 09:57:00 -0700

    disable DBG_ENITITY_10017_POSITION_CHANGES

--- src/flags.ts
+++ src/flags.ts
@@ -44,7 +44,7 @@ export const WARN_DEAD_CLEANUP = false;
 export const DBG_SYSTEM_ORDER = false;
 // prints when 'PositionDef' on '10017' changes after a system call, init fn, or entity promise
 // TODO(@darzu): GENERALIZE THIS! for other entities and components
-export const DBG_ENITITY_10017_POSITION_CHANGES = true;
+export const DBG_ENITITY_10017_POSITION_CHANGES = false;
 
 // Input
 export const DEBUG_INPUTS = false;
