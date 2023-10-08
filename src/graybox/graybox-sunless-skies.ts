3cf0fd2828f59d41417a24114d7fc850f87c7dde
darzu <daryl.zuniga@gmail.com>
2023-10-08 13:45:51 -0700

    starting sunless skies graybox

---
+++ src/graybox/graybox-sunless-skies.ts
@@ -0,0 +1,30 @@
+/*
+sketch:
+
+environment
+  world plane
+docks
+ship
+pirates
+
+gameplay:
+  front cannon
+  docking
+  trade goods
+  upgrades:
+    broadside cannons
+    relative tracking
+    thruster speed
+
+PSEUDO CODE:
+
+init
+  initEnvironment
+  initShip
+
+initEnvironment
+  createWalls
+  createDocks
+
+    
+*/
