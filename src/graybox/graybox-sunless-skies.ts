34fa00d4e9b860274e7be96d1480b45f19856c07
darzu <daryl.zuniga@gmail.com>
2023-10-08 14:17:42 -0700

    30min on graybox

--- src/graybox/graybox-sunless-skies.ts
+++ src/graybox/graybox-sunless-skies.ts
@@ -1,4 +1,6 @@
 /*
+# of Sessions: 1
+
 sketch:
 
 environment
@@ -10,21 +12,71 @@ pirates
 gameplay:
   front cannon
   docking
+    slide into dock
   trade goods
+    each dock randomly generates trade missions with timers,
+    each dock restocks temp trade goods w/ timer,
+    each dock has a set of reliable resource forever, but more expensive,
   upgrades:
     broadside cannons
     relative tracking
     thruster speed
+  world danger:
+    enemy ships have headlights and smoke trails,
+      gives you a heads up when u might encounter them
+    
+
+
+
 
 PSEUDO CODE:
 
-init
+init:
   initEnvironment
   initShip
+  initShipVsShip
+  initDocks
+  initTradeRoutes
 
-initEnvironment
+initEnvironment:
   createWalls
   createDocks
+  spawnEnemies
+
+initShip:
+  setRenderable
+  setPosition
+  setVelocity
+  setShipHeat
+  
+  on update:
+    // move
+    accel = f(inputs, shipHeat)
+    vel += accel
+    vel -= dampening(vel)
+  
+  on spacebar:
+    // try fire
+    kickBack
+
+initShipVsShip:
+  on collision(shipA, shipB):
+    dmg(shipA)
+    dmg(shipB)
+    pushBack(shipA)
+    pushBack(shipB)
+
+  on collision(ship, bullet)
+    dmg(ship)
+    pushBack(ship)
+    delete(bullet)
+  
+initDocks:
+  for each dock pos:
+    createDock
+
+  
+
 
     
 */
