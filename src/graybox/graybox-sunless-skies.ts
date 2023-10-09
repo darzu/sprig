83f3dc2702bd505ff0a95cd68c9cf04714141113
darzu <daryl.zuniga@gmail.com>
2023-10-09 10:42:20 -0700

    session 2

--- src/graybox/graybox-sunless-skies.ts
+++ src/graybox/graybox-sunless-skies.ts
@@ -1,7 +1,7 @@
 /*
-# of Sessions: 1
+# of Sessions: 2
 
-sketch:
+SKETCH:
 
 environment
   world plane
@@ -24,10 +24,31 @@ gameplay:
   world danger:
     enemy ships have headlights and smoke trails,
       gives you a heads up when u might encounter them
+    graph of possible enemy travel
+      enemies pick random new location to travel to
+      dead enemies respawn after X seconds
+  enemy AI:
+    moves toward destination by picking random point on circle in radius R of destination
+      draw line from here to there, if unobstructed, that is the path
+      waits for velocity drift to near zero before aiming and moving
+    aggro bubble
+      pulls enemy cluster
+    enemy moves toward player
+      goal is to be within X distance + facing the player
+      shoots when it can
+    periodically adjusts to look at the player
+      has a %chance of looking aiming correctly
+        takes into account velocity (not accel)
+        chance is worse as velocity increases
+      longer the player stays still, more likely they'll be looking at the player
+      if player is moving, chance are much lower
+    fires when it's roughly looking at the player, every N seconds + fudge
+    occassionally strafes before aiming
+    movement is based on target radius from player
+      aims toward nearest unobstructed point on circle and moves toward
+    flees at Z% health
+
     
-
-
-
 
 PSEUDO CODE:
 
@@ -52,6 +73,8 @@ initShip:
   on update:
     // move
     accel = f(inputs, shipHeat)
+      // strafe costs heat
+      // boost costs heat
     vel += accel
     vel -= dampening(vel)
   
