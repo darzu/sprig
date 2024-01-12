e8b5ef6527ed4257f4308aa138fd0f82d7837132
darzu <daryl.zuniga@gmail.com>
2024-01-12 15:15:26 -0800

    child objects as ComponentDef[] or entities

--- src/graybox/objects.ts
+++ src/graybox/objects.ts
@@ -1,5 +1,5 @@
 import { ComponentDef, EM, EntityW } from "../ecs/entity-manager.js";
-import { V } from "../matrix/sprig-matrix.js";
+import { V, quat, vec3 } from "../matrix/sprig-matrix.js";
 import { CubeMesh } from "../meshes/mesh-list.js";
 import { PositionDef, ScaleDef, RotationDef } from "../physics/transform.js";
 import { RenderableConstructDef } from "../render/renderer-ecs.js";
@@ -41,12 +41,21 @@ an object has:
 // TODO(@darzu): NON-OBJ interop: children needs to accept entities instead of objects and built objects instead of obj def
 // TODO(@darzu): self-props is optional? just creates an EntityW<> in that case?
 
+// TODO(@darzu): ARRAY OF CHILDREN?
+//    instead of children needing names and a custom component, just use indexes which can
+//    be strongly typed
+
+type ObjChildDef = ObjDef | readonly ComponentDef[];
+type ObjChildOpt = ObjOpt | ObjChildDef;
+
+type CType = undefined | Record<string, ObjChildOpt>;
+
 // defines an "object" which is an entity w/ a set of components
 //   and children objects
-export interface ObjDefOpts<
+export interface ObjOpt<
   N extends string = string,
   CS extends readonly ComponentDef[] = any[],
-  C extends undefined | Record<string, ObjDefOpts | ObjDef> = {},
+  C extends CType = {},
   P extends object = any
   // CArgs extends any[] = any,
   // UArgs extends any[] = any
@@ -67,76 +76,107 @@ export interface ObjDefOpts<
   children?: C;
 }
 
-export interface ObjDef<D extends ObjDefOpts = ObjDefOpts> {
+export interface ObjDef<D extends ObjOpt = ObjOpt> {
   opts: D;
   props: ObjComponentDef<D>;
-  children: Record<string, ObjDef>;
+  children: Record<string, ObjChildDef>;
 }
 
-function isObjDef(d: ObjDef | ObjDefOpts): d is ObjDef {
+function isCompDefs(d: ObjChildOpt): d is readonly ComponentDef[] {
+  return Array.isArray(d);
+}
+
+function isObjDef(d: ObjDef | ObjOpt): d is ObjDef {
   return "opts" in d;
 }
 
-export type ObjOwnProps<D extends ObjDef> = D extends ObjDef<infer O>
-  ? O extends ObjDefOpts<any, any, any, infer P>
-    ? P
-    : never
-  : never;
-
 // helper to grab opts from def or opts
-type ObjOpts<D extends ObjDef | ObjDefOpts> = D extends ObjDef ? D["opts"] : D;
+type ObjPickOpt<D extends ObjDef | ObjOpt> = D extends ObjDef ? D["opts"] : D;
+
+export type ObjOwnProps<D extends ObjDef | ObjOpt> =
+  ObjPickOpt<D> extends ObjOpt<any, any, any, infer P> ? P : never;
 
 // the component def the tracks the children and custom data of the object
-type _ObjComponentP<
-  C extends undefined | Record<string, ObjDefOpts | ObjDef>,
-  P extends Object
-> = C extends Record<string, ObjDefOpts | ObjDef>
-  ? { [n in keyof C]: _ObjEnt<ObjOpts<C[n]>> } & P
+type ObjComponentP<C extends CType, P extends Object> = C extends Record<
+  string,
+  ObjChildOpt
+>
+  ? { [n in keyof C]: ObjChildEnt<C[n]> } & P
   : P;
-type ObjComponentDef<D extends ObjDefOpts> = D extends ObjDefOpts<
+type ObjComponentDef<D extends ObjOpt> = D extends ObjOpt<
   infer N,
   any,
   infer C,
   infer P
 >
-  ? ComponentDef<N, _ObjComponentP<C, P>, [ObjArgs<D>], []>
+  ? ComponentDef<N, ObjComponentP<C, P>, [ObjArgs<D>], []>
   : never;
 
 // the entity and all components of an object
-type _ObjEnt<D extends ObjDefOpts = ObjDefOpts> = D extends ObjDefOpts<
-  any,
-  infer CS
->
-  ? EntityW<[ObjComponentDef<D>, ...CS]>
+export type ObjEnt<D extends ObjOpt | ObjDef = ObjOpt | ObjDef> =
+  ObjPickOpt<D> extends ObjOpt<any, infer CS>
+    ? EntityW<[ObjComponentDef<ObjPickOpt<D>>, ...CS]>
+    : never;
+
+type ObjChildEnt<CO extends ObjChildOpt = ObjChildOpt> = CO extends
+  | ObjOpt
+  | ObjDef
+  ? ObjEnt<CO>
+  : CO extends readonly ComponentDef[]
+  ? EntityW<CO>
   : never;
-export type ObjEnt<D extends ObjDefOpts | ObjDef> = _ObjEnt<ObjOpts<D>>;
 
 // the arguments needed to construct an object
-type _ObjArgs<D extends ObjDefOpts> = D extends ObjDefOpts<any, infer CS>
+type _CompArgs<C extends ComponentDef> = C extends ComponentDef<
+  any,
+  any,
+  infer CArgs,
+  infer UArgs
+>
+  ? [...CArgs, ...UArgs]
+  : never;
+type _CompName<C extends ComponentDef> = C extends ComponentDef<infer N>
+  ? N
+  : never;
+
+type _ObjCSArgs<D extends ObjOpt> = D extends ObjOpt<any, infer CS>
   ? Intersect<{
-      [i in keyof CS]: CS[i] extends ComponentDef<
-        infer N,
-        any,
-        infer CArgs,
-        infer UArgs
-      >
-        ? { [_ in N]: [...CArgs, ...UArgs] }
-        : never;
+      [i in keyof CS]: { [N in _CompName<CS[i]>]: _CompArgs<CS[i]> };
     }>
   : undefined;
-export type ObjArgs<D extends ObjDefOpts = ObjDefOpts> = D extends ObjDefOpts<
+
+type _CompArrayArgs<CS extends readonly ComponentDef[]> = {
+  [i in keyof CS]: _CompArgs<CS[i]>;
+};
+
+type _ObjChildArg<CO extends ObjChildOpt = ObjChildOpt> = CO extends
+  | ObjOpt
+  | ObjDef
+  ? ObjArgs<ObjPickOpt<CO>>
+  : CO extends readonly ComponentDef[]
+  ? _CompArrayArgs<CO>
+  : never;
+type ObjChildArg<CO extends ObjChildOpt = ObjChildOpt> =
+  | _ObjChildArg<CO>
+  | ObjChildEnt<CO>;
+
+function isObjChildEnt(ca: _ObjChildArg | ObjChildEnt): ca is ObjChildEnt {
+  return "id" in ca;
+}
+
+export type ObjArgs<D extends ObjOpt = ObjOpt> = D extends ObjOpt<
   any,
   any,
   infer C,
   infer P
 >
   ? {
-      args: _ObjArgs<D>;
+      args: _ObjCSArgs<D>;
     } & (C extends Record<any, any>
       ? {
-          children: C extends Record<any, any>
+          children: C extends Record<any, ObjChildOpt>
             ? {
-                [n in keyof C]: ObjArgs<ObjOpts<C[n]>>;
+                [n in keyof C]: ObjChildArg<C[n]>;
               }
             : undefined;
         }
@@ -153,35 +193,47 @@ export type ObjArgs<D extends ObjDefOpts = ObjDefOpts> = D extends ObjDefOpts<
 export function defineObj<
   N extends string,
   CS extends readonly ComponentDef[],
-  C extends undefined | Record<string, ObjDefOpts | ObjDef>,
+  C extends CType,
   P extends object
   // O extends ObjDefOpts<N, CS, C, P> = ObjDefOpts<N, CS, C, P>
   // CArgs extends any[],
   // UArgs extends any[]
   // >(opts: O): ObjDef<O> {
->(opts: ObjDefOpts<N, CS, C, P>): ObjDef<ObjDefOpts<N, CS, C, P>> {
-  type O = ObjDefOpts<N, CS, C, P>;
+>(opts: ObjOpt<N, CS, C, P>): ObjDef<ObjOpt<N, CS, C, P>> {
+  type O = ObjOpt<N, CS, C, P>;
 
   // define children
-  const childDefs: Record<string, ObjDef> = {};
+  const childDefs: Record<string, ObjChildDef> = {};
   if (opts.children) {
     for (let cName of Object.keys(opts.children)) {
-      const defOrOpts = opts.children[cName];
-      childDefs[cName] = isObjDef(defOrOpts) ? defOrOpts : defineObj(defOrOpts);
+      const defOrOptsOrCS: ObjChildOpt = opts.children[cName];
+      if (isCompDefs(defOrOptsOrCS)) {
+        childDefs[cName] = defOrOptsOrCS;
+      } else if (isObjDef(defOrOptsOrCS)) {
+        childDefs[cName] = defOrOptsOrCS;
+      } else {
+        childDefs[cName] = defineObj(defOrOptsOrCS);
+      }
     }
   }
 
   const createChildrenObjsAndProps = function (
     args: ObjArgs<O>
-  ): _ObjComponentP<C, P> {
+  ): ObjComponentP<C, P> {
     // create children objects
-    const childEnts: Record<string, _ObjEnt> = {};
+    const childEnts: Record<string, ObjChildEnt> = {};
     if (args.children) {
       for (let cName of Object.keys(args.children)) {
-        const cArgs: ObjArgs = args.children[cName];
-        const cDef: ObjDef = childDefs[cName];
-        const cEnt = createObj(cDef, cArgs);
-        childEnts[cName] = cEnt;
+        const cArgs: ObjChildArg = args.children[cName];
+        if (isObjChildEnt(cArgs)) {
+          // already an entity
+          childEnts[cName] = cArgs;
+        } else {
+          // create the entity
+          const cDef: ObjChildDef = childDefs[cName];
+          const cEnt = createChildObj(cDef, cArgs);
+          childEnts[cName] = cEnt;
+        }
       }
     }
 
@@ -191,7 +243,7 @@ export function defineObj<
     const res = {
       ...childEnts,
       ...p,
-    } as _ObjComponentP<C, P>;
+    } as ObjComponentP<C, P>;
 
     return res;
   };
@@ -211,10 +263,17 @@ export function defineObj<
   return def;
 }
 
+export function createChildObj<D extends ObjChildDef, A extends ObjChildArg<D>>(
+  def: D,
+  args: A
+): ObjChildEnt<D> {
+  throw "TODO";
+}
+
 export function createObj<D extends ObjDef, A extends ObjArgs<D["opts"]>>(
   def: D,
   args: A
-): _ObjEnt<D["opts"]> {
+): ObjEnt<D["opts"]> {
   // create entity
   const e = EM.new();
 
@@ -229,7 +288,7 @@ export function createObj<D extends ObjDef, A extends ObjArgs<D["opts"]>>(
   EM.set(e, def.props, args);
 
   // TODO(@darzu): there's probably some extreme type-foo that could do this impl w/o cast
-  return e as _ObjEnt<D["opts"]>;
+  return e as ObjEnt<D["opts"]>;
 }
 
 export function T<N extends {}>(): (p: N) => void {
@@ -263,16 +322,17 @@ export function testObjectTS() {
       },
       cannonL: CannonObj,
       cannonR: CannonObj,
+      rudder: [PositionDef, RotationDef],
     },
   } as const);
 
   type __t0 = (typeof ShipObj)["opts"];
   type __t5 = (typeof ShipObj)["opts"]["children"];
-  type __t3<D extends ObjDefOpts> = D extends ObjDefOpts<infer N, any, infer C>
+  type __t3<D extends ObjOpt> = D extends ObjOpt<infer N, any, infer C>
     ? // Intersect<{ [i in keyof COS]: { [_ in COS[i][0]]: Obj<COS[i][1]> } }>
       // { [i in keyof COS]: { [_ in COS[i][0]]: Obj<COS[i][1]> } }
       {
-        [n in keyof C]: C[n] extends ObjDefOpts<infer CN, infer CS>
+        [n in keyof C]: C[n] extends ObjOpt<infer CN, infer CS>
           ? // ? Obj<ObjDefinition<N2, [...CS]>>
             EntityW<[...CS]>
           : never;
@@ -283,25 +343,24 @@ export function testObjectTS() {
   type __t4 = __t3<__t0>;
 
   if (!"true") {
-    let __o4 = null as unknown as __t4;
-    const l23 = __o4.mast.scale;
+    // let __o4 = null as unknown as __t4;
+    // const l23 = __o4.mast.scale;
   }
 
   type __t1 = ObjComponentDef<__t0>;
-  type __t2 = _ObjEnt<__t0>;
+  // type __t2 = _ObjEnt<__t0>;
   // type __t1 = ReturnType<typeof createObject<typeof ShipObj>>;
 
-  type __t6<D extends ObjDefOpts> = D extends ObjDefOpts<
-    infer N,
-    any,
-    infer C,
-    infer P
-  >
+  type __t6<D extends ObjOpt> = D extends ObjOpt<infer N, any, infer C, infer P>
     ? P
     : never;
   type __t7 = __t6<__t0>;
 
   type __t8 = ObjArgs<__t0>["children"];
+
+  type __t9 = ObjDef | ObjOpt;
+  type __t10 = ObjPickOpt<__t9>;
+  type __t11 = ObjArgs<__t10>;
 
   console.log("testGrayHelpers".toUpperCase());
   console.dir(ShipObj);
@@ -336,16 +395,32 @@ export function testObjectTS() {
           position: [V(1, 0, 0)],
         },
       },
+      rudder: [[[1, 1, 1]], []],
     },
   });
   console.dir(ship);
+
+  let foo = "klj" as string | undefined;
+  let bar = foo?.endsWith("j");
 
   ship.ship.myProp = 8;
   ship.position;
   // const cl = ship.ship["cannonL"];
   const cl = ship.ship.cannonL;
-  const m = ship.ship.mast.mast2.sail;
-  const mp = m.rotation;
+  const se = ship.ship.mast.mast2.sail;
+  const mp: quat = se.rotation;
+
+  const cannonLPos: vec3 = ship.ship.cannonL.position;
+  const rudderPos: vec3 = ship.ship.rudder.position;
+
+  // TODO(@darzu): oo i like this one best
+  // const cl3 = ship.child.cannonL;
+  // const se3 = ship.child.mast.child.sail;
+  // const mp3 = se.rotation;
+
+  // const cl2 = ship.child[0];
+  // const se2 = ship.child[1].child[0];
+  // const mp2 = m.rotation;
 
   // const ShipDef = defineObject("ship", {
   //   position: [V(0,0,0)],
