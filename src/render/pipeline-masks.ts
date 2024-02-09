b89f64caa778c4417f757172f06d6532d77221ee
darzu <daryl.zuniga@gmail.com>
2024-02-09 09:10:29 -0800

    fix masks

--- src/render/pipeline-masks.ts
+++ src/render/pipeline-masks.ts
@@ -2,10 +2,10 @@
 
 // TODO(@darzu): implement a proper material system
 
-export const DEFAULT_MASK = 1 << 0;
-export const UVUNWRAP_MASK = 1 << 1;
-export const ALPHA_MASK = 1 << 2;
-export const SKY_MASK = 1 << 3;
-export const BUBBLE_MASK = 1 << 4;
-export const GRID_MASK = 1 << 5;
-export const JFA_PRE_PASS_MASK = 1 << 6;
+export const DEFAULT_MASK       = 1 << 0; // prettier-ignore
+export const UVUNWRAP_MASK      = 1 << 1; // prettier-ignore
+export const ALPHA_MASK         = 1 << 2; // prettier-ignore
+export const SKY_MASK           = 1 << 3; // prettier-ignore
+export const BUBBLE_MASK        = 1 << 4; // prettier-ignore
+export const GRID_MASK          = 1 << 5; // prettier-ignore
+export const JFA_PRE_PASS_MASK  = 1 << 6; // prettier-ignore
