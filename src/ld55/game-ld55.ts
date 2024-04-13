6555a434fe952ef67d1deba0b0bf301bdcf7a2b5
darzu <daryl.zuniga@gmail.com>
2024-04-13 09:51:55 -0700

    starting ld55

---
+++ src/ld55/game-ld55.ts
@@ -0,0 +1,123 @@
+import { CameraDef } from "../camera/camera.js";
+import { ColorDef } from "../color/color-ecs.js";
+import { ENDESGA16 } from "../color/palettes.js";
+import { EM } from "../ecs/entity-manager.js";
+import { createSun, initGhost } from "../graybox/graybox-helpers.js";
+import { createObj } from "../graybox/objects.js";
+import { V, V3 } from "../matrix/sprig-matrix.js";
+import { HexMesh, PlaneMesh } from "../meshes/mesh-list.js";
+import { HEX_AABB, mkCubeMesh } from "../meshes/primatives.js";
+import { MeDef } from "../net/components.js";
+import { ColliderDef } from "../physics/collider.js";
+import { PositionDef, ScaleDef } from "../physics/transform.js";
+import { GRID_MASK } from "../render/pipeline-masks.js";
+import { deferredPipeline } from "../render/pipelines/std-deferred.js";
+import { stdGridRender } from "../render/pipelines/std-grid.js";
+import {
+  lineMeshPoolPtr,
+  linePipe,
+  pointPipe,
+} from "../render/pipelines/std-line.js";
+import { stdMeshPipe } from "../render/pipelines/std-mesh.js";
+import { outlineRender } from "../render/pipelines/std-outline.js";
+import { postProcess } from "../render/pipelines/std-post.js";
+import { shadowPipelines } from "../render/pipelines/std-shadow.js";
+import { RendererDef, RenderableConstructDef } from "../render/renderer-ecs.js";
+import { sketch } from "../utils/sketch.js";
+import { addWorldGizmo } from "../utils/utils-game.js";
+
+const DBG_GHOST = true;
+
+export async function initLd55() {
+  stdGridRender.fragOverrides!.lineSpacing1 = 8.0;
+  stdGridRender.fragOverrides!.lineWidth1 = 0.05;
+  stdGridRender.fragOverrides!.lineSpacing2 = 256;
+  stdGridRender.fragOverrides!.lineWidth2 = 0.2;
+  stdGridRender.fragOverrides!.ringStart = 512;
+  stdGridRender.fragOverrides!.ringWidth = 0;
+
+  EM.addEagerInit([], [RendererDef], [], (res) => {
+    // renderer
+    res.renderer.pipelines = [
+      ...shadowPipelines,
+      stdMeshPipe,
+      outlineRender,
+      deferredPipeline,
+      pointPipe,
+      linePipe,
+
+      stdGridRender,
+
+      postProcess,
+    ];
+  });
+
+  const { camera, me } = await EM.whenResources(CameraDef, MeDef);
+
+  // camera
+  camera.fov = Math.PI * 0.5;
+  camera.viewDist = 1000;
+  V3.set(-200, -200, -200, camera.maxWorldAABB.min);
+  V3.set(+200, +200, +200, camera.maxWorldAABB.max);
+
+  // sun
+  createSun();
+
+  // grid
+  const grid = createObj(
+    [RenderableConstructDef, PositionDef, ScaleDef, ColorDef] as const,
+    {
+      renderableConstruct: [PlaneMesh, true, undefined, GRID_MASK],
+      position: [0, 0, 0],
+      scale: [2 * camera.viewDist, 2 * camera.viewDist, 1],
+      // color: [0, 0.5, 0.5],
+      color: [0.5, 0.5, 0.5],
+      // color: [1, 1, 1],
+    }
+  );
+
+  // pedestal
+  const pedestal = EM.new();
+  EM.set(pedestal, RenderableConstructDef, HexMesh);
+  EM.set(pedestal, ColorDef, ENDESGA16.darkGray);
+  EM.set(pedestal, PositionDef, V(0, 0, -10));
+  EM.set(pedestal, ScaleDef, V(10, 10, 10));
+  EM.set(pedestal, ColliderDef, {
+    shape: "AABB",
+    solid: true,
+    aabb: HEX_AABB,
+  });
+
+  // gizmo
+  addWorldGizmo(V(0, 0, 0), 5);
+
+  // line box
+  const lineBox = createObj(
+    [RenderableConstructDef, PositionDef, ColorDef, ScaleDef] as const,
+    {
+      renderableConstruct: [
+        mkCubeMesh(),
+        true,
+        undefined,
+        undefined,
+        lineMeshPoolPtr,
+      ],
+      position: [10, 10, 10],
+      scale: [5, 5, 5],
+      color: ENDESGA16.lightGreen,
+    }
+  );
+
+  // line test
+  sketch({
+    shape: "line",
+    color: ENDESGA16.orange,
+    start: [-10, -10, -10],
+    end: [10, 10, 10],
+  });
+
+  // dbg ghost
+  if (DBG_GHOST) {
+    initGhost();
+  }
+}
