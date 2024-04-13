ea1d92cf11c25f27c4960663a05c8ebaa803cde0
darzu <daryl.zuniga@gmail.com>
2024-04-13 15:28:10 -0700

    starting summon circle jfa

--- src/ld55/game-ld55.ts
+++ src/ld55/game-ld55.ts
@@ -1,20 +1,26 @@
 import { CameraDef } from "../camera/camera.js";
 import { ColorDef } from "../color/color-ecs.js";
 import { ENDESGA16 } from "../color/palettes.js";
+import { DevConsoleDef } from "../debug/console.js";
 import { EM } from "../ecs/entity-manager.js";
 import { Phase } from "../ecs/sys-phase.js";
 import { createSun, initGhost } from "../graybox/graybox-helpers.js";
 import { createObj } from "../graybox/objects.js";
-import { V, V2, V3 } from "../matrix/sprig-matrix.js";
-import { BallMesh, HexMesh, PlaneMesh } from "../meshes/mesh-list.js";
+import { V, V2, V3, V4 } from "../matrix/sprig-matrix.js";
+import { BallMesh, CubeMesh, HexMesh, PlaneMesh } from "../meshes/mesh-list.js";
 import { HEX_AABB, mkCubeMesh } from "../meshes/primatives.js";
 import { MeDef } from "../net/components.js";
 import { ColliderDef } from "../physics/collider.js";
 import { PositionDef, ScaleDef } from "../physics/transform.js";
+import { createRenderTextureToQuad } from "../render/gpu-helper.js";
+import { CY } from "../render/gpu-registry.js";
 import { GRID_MASK } from "../render/pipeline-masks.js";
+import { createGridComposePipelines } from "../render/pipelines/std-compose.js";
 import { deferredPipeline } from "../render/pipelines/std-deferred.js";
 import { stdGridRender } from "../render/pipelines/std-grid.js";
+import { createJfaPipelines } from "../render/pipelines/std-jump-flood.js";
 import {
+  LineUniDef,
   lineMeshPoolPtr,
   linePipe,
   pointPipe,
@@ -23,7 +29,11 @@ import { stdMeshPipe } from "../render/pipelines/std-mesh.js";
 import { outlineRender } from "../render/pipelines/std-outline.js";
 import { postProcess } from "../render/pipelines/std-post.js";
 import { shadowPipelines } from "../render/pipelines/std-shadow.js";
-import { RendererDef, RenderableConstructDef } from "../render/renderer-ecs.js";
+import {
+  RendererDef,
+  RenderableConstructDef,
+  RenderableDef,
+} from "../render/renderer-ecs.js";
 import {
   sketch,
   sketchDot,
@@ -38,6 +48,11 @@ import { addWorldGizmo } from "../utils/utils-game.js";
 
 const DBG_GHOST = true;
 
+const radius = 10;
+const diam = radius * 2;
+
+const radiusPlusWidth = radius + 6;
+
 export const GamepadDef = EM.defineResource("gamepad", () => {
   return {
     leftStick: V(0, 0),
@@ -47,6 +62,111 @@ export const GamepadDef = EM.defineResource("gamepad", () => {
   };
 });
 
+const shader_lineJfaMask = `
+struct VertexOutput {
+  @builtin(position) fragPos : vec4<f32>,
+}
+
+@vertex
+fn vertMain(input: VertexInput) -> VertexOutput {
+  var output: VertexOutput;
+  let worldPos = meshUni.transform * vec4<f32>(input.position, 1.0);
+
+  let x = (worldPos.x / ${radiusPlusWidth}); // -1,1
+  let y = (worldPos.y / ${radiusPlusWidth}); // -1,1
+
+  output.fragPos = vec4(x, y, 0.0, 1.0);
+  return output;
+}
+
+struct FragOut {
+  @location(0) color: f32,
+  // @location(0) color: vec4<f32>,
+}
+
+@fragment fn fragMain(input: VertexOutput) -> FragOut {
+  var output: FragOut;
+  output.color = 1.0;
+  // output.color = vec4(1.0);
+  return output;
+}
+`;
+
+const jfaMaskTex = CY.createTexture("summonMaskTex", {
+  size: [diam * 128, diam * 128],
+  format: "r8unorm",
+});
+console.log("summonMaskTex.size:" + jfaMaskTex.size[0]);
+
+const jfaMaskLineRender = CY.createRenderPipeline("jfaMaskLineRender", {
+  globals: [],
+  shader: () => `
+  ${shader_lineJfaMask}
+  `,
+  shaderVertexEntry: "vertMain",
+  shaderFragmentEntry: "fragMain",
+  meshOpt: {
+    pool: lineMeshPoolPtr,
+    // meshMask: FONT_JFA_MASK,
+    stepMode: "per-mesh-handle",
+  },
+  topology: "line-list",
+  cullMode: "none",
+  output: [
+    {
+      ptr: jfaMaskTex,
+      clear: "once",
+      // defaultColor: V4.clone([0.1, 0.1, 0.1, 0.0]),
+      defaultColor: V4.clone([0.0, 0.0, 0.0, 0.0]),
+    },
+  ],
+});
+
+const summonJfa = createJfaPipelines({
+  maskTex: jfaMaskTex,
+  maskMode: "interior",
+  sdfDistFact: 10.0,
+  maxDist: 512,
+  size: 512 * 8,
+});
+
+const summonSdfExampleTex = CY.createTexture("summonSdfExampleTex", {
+  size: jfaMaskTex.size,
+  format: "r8unorm",
+  // format: "r16float",
+  // format: "rgba8unorm",
+});
+
+console.log("summonSdfExampleTex.size:" + summonSdfExampleTex.size[0]);
+const pipeSummonJfaLineSdfExample = createRenderTextureToQuad(
+  "pipeSummonJfaLineSdfExample",
+  summonJfa.sdfTex,
+  summonSdfExampleTex,
+  -1,
+  1,
+  -1,
+  1,
+  true,
+  () => `
+    // let c = textureLoad(inTex, xy, 0).x;
+    // let c = textureSample(inTex, samp, uv).x;
+    let c = inPx;
+    // if (c < 0.05) {
+    return 1.0 - smoothstep(0.03, 0.05, c);
+      // return 1.0;
+    // } else {
+    //   return 0.0;
+    // }
+  `
+).pipeline;
+
+// prittier-ignore
+const dbgGrid = [
+  [summonJfa._inputMaskTex, summonJfa._uvMaskTex],
+  [summonJfa.sdfTex, summonSdfExampleTex],
+];
+let dbgGridCompose = createGridComposePipelines(dbgGrid);
+
 export async function initLd55() {
   stdGridRender.fragOverrides!.lineSpacing1 = 8.0;
   stdGridRender.fragOverrides!.lineWidth1 = 0.05;
@@ -55,21 +175,51 @@ export async function initLd55() {
   stdGridRender.fragOverrides!.ringStart = 512;
   stdGridRender.fragOverrides!.ringWidth = 0;
 
-  EM.addEagerInit([], [RendererDef], [], (res) => {
-    // renderer
-    res.renderer.pipelines = [
-      ...shadowPipelines,
-      stdMeshPipe,
-      outlineRender,
-      deferredPipeline,
-      pointPipe,
-      linePipe,
+  let didDraw = false;
 
-      stdGridRender,
+  let _frame = 0; // TODO(@darzu): HACK. idk what the dependency is..
+  EM.addSystem(
+    "summonLineJfa",
+    Phase.GAME_WORLD,
+    [LineUniDef, RenderableDef],
+    [RendererDef],
+    (es, res) => {
+      _frame++;
+      if (_frame > 2 && !didDraw) return;
 
-      postProcess,
-    ];
-  });
+      const handles = es.map((e) => e.renderable.meshHandle);
+
+      res.renderer.renderer.submitPipelines(handles, [
+        jfaMaskLineRender,
+        ...summonJfa.allPipes(),
+        pipeSummonJfaLineSdfExample,
+      ]);
+    }
+  );
+
+  EM.addSystem(
+    "ld55Pipelines",
+    Phase.GAME_WORLD,
+    null,
+    [RendererDef, DevConsoleDef],
+    (_, res) => {
+      // renderer
+      res.renderer.pipelines = [
+        ...shadowPipelines,
+        stdMeshPipe,
+        outlineRender,
+        deferredPipeline,
+        pointPipe,
+        linePipe,
+
+        stdGridRender,
+
+        postProcess,
+
+        ...(res.dev.showConsole ? dbgGridCompose : []),
+      ];
+    }
+  );
 
   const { camera, me } = await EM.whenResources(CameraDef, MeDef);
 
@@ -97,7 +247,7 @@ export async function initLd55() {
 
   // pedestal
   const pedestal = EM.new();
-  EM.set(pedestal, RenderableConstructDef, HexMesh);
+  EM.set(pedestal, RenderableConstructDef, CubeMesh);
   EM.set(pedestal, ColorDef, ENDESGA16.darkGray);
   EM.set(pedestal, PositionDef, V(0, 0, -10));
   EM.set(pedestal, ScaleDef, V(20, 20, 10));
@@ -140,8 +290,6 @@ export async function initLd55() {
     initGhost();
   }
 
-  const radius = 10;
-  const diam = radius * 2;
   sketchSvg(
     [
       { i: "M", x: -radius, y: 0 },
@@ -185,6 +333,8 @@ export async function initLd55() {
     null,
     [GamepadDef],
     (_, { gamepad }) => {
+      didDraw = false;
+
       leftDot.position[0] = gamepad.leftStick[0] * radius;
       leftDot.position[1] = gamepad.leftStick[1] * radius;
 
@@ -212,6 +362,7 @@ export async function initLd55() {
           sketchLine(left, right, {
             color: ENDESGA16.lightGreen,
           });
+          didDraw = true;
         }
       }
     }
