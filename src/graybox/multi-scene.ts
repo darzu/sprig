dbdac2e13de822dadbd8f4000b68da1b6054d767
darzu <daryl.zuniga@gmail.com>
2024-04-24 10:54:17 -0700

    notes on multi-scene

---
+++ src/graybox/multi-scene.ts
@@ -0,0 +1,31 @@
+/*
+multi-scene
+
+pipelines
+disable old scene entities (take out of systems lists, take out of renderer)
+enable new scene entities 
+
+push scene
+pop scene
+
+optional marking/annotations on systems and entities ?
+
+active / deactive scene
+
+resource tagging
+
+or worlds:
+  all resources attached to a world
+  world swapped loaded/unloaded,
+    all caches swapped
+
+maybe resources are shared
+everything else isn't
+
+goal:
+  scene swapping is useful for game too
+    swap missions areas, store, etc.
+
+
+
+*/
