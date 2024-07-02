a8ccd7f9b4cdd718c0c713123020d47eb422635a
darzu <daryl.zuniga@gmail.com>
2024-07-02 11:46:18 -0700

    exclude symlinks from copyToSprigLand

--- copyToSprigLand.sh
+++ copyToSprigLand.sh
@@ -2,7 +2,7 @@
 cd ~/sprig
 HASH=`git rev-parse --short HEAD`
 cd ~/darzu.github.io
-rsync -rav --exclude=".*" --ignore-errors ~/sprig/public/ ~/darzu.github.io/
+rsync -rav --no-links --exclude=".*" --ignore-errors ~/sprig/public/ ~/darzu.github.io/
 # cp -r ~/sprig/public/*.js ~/ld54/ #problems w/ .git folder 
 git add --all .
 git commit -m "from sprig $HASH"
